7b5b6faec1ec9490a51ab70bfaa5757e98fd3bfd
monicagirgis <mocagirgis222@gmail.com>
2022-01-06 10:25:28 +0200

    fix ui issues

---
+++ HomePage/Scenes/Home/views/HorizontalCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  HorizontalCollectionViewCell.swift
+//  HomePage
+//
+//  Created by Monica Girgis Kamel on 06/01/2022.
+//
+
+import UIKit
+
+class HorizontalCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ HomePage/Scenes/Home/views/LinesCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  LinesCollectionViewCell.swift
+//  HomePage
+//
+//  Created by Monica Girgis Kamel on 06/01/2022.
+//
+
+import UIKit
+
+class LinesCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
